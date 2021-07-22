5fb07bc4e82e4d4ee3cba1a403a26ae4b919a4be
SunPointed <lqy410338643@gmail.com>
2021-07-22 22:04:33 +0800

    Initial Commit

---
+++ learn/learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnApp.swift
+//  learn
+//
+//  Created by 罗秋雨 on 2021/7/22.
+//
+
+import SwiftUI
+
+@main
+struct learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
